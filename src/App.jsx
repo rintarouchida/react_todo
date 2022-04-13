2b76da3ab8888624702b9d45872d8ec3880eb02e
rintarouchida <ww.stamford@gmail.com>
2022-04-13 22:14:09 +0900

    make_component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import React, {useState} from 'react';
 import './style.css';
+import {CompleteTodos} from './components/CompleteTodos';
+import {InputTodo} from './components/InputTodo';
+import {IncompleteTodos} from './components/IncompleteTodos';
 
 export const App = () => {
   const [todoText, setTodoText] = useState('');
@@ -43,39 +46,9 @@ export const App = () => {
   }
   return(
     <React.Fragment>
-      <div className="input-area">
-        <input placeholder="TODOを入力" value={todoText} onChange={onChangeTodoText}></input>
-        <button onClick={onClickAdd}>追加</button>
-      </div>
-
-      <div className="incomplete-area">
-        <p className="title">未完了のTODO</p>
-        <ul>
-          {incompleteTodos.map((todo, index) => {
-            return (
-              <div className="list-row" key={todo}>
-                <li>{todo}</li>
-                <button onClick={() => onClickComplete(index)}>完了</button>
-                <button onClick={() => onClickDelete(index)}>削除</button>
-              </div>
-            );
-          })}
-        </ul>
-      </div>
-
-      <div className="complete-area">
-      <p className="title">完了のTODO</p>
-        <ul>
-          {completeTodos.map((todo,index) => {
-            return (
-              <div className="list-row" key={todo}>
-                <li>{todo}</li>
-                <button onClick={() => onClickBack(index)}>戻す</button>
-              </div>
-            );
-          })}
-        </ul>
-      </div>
+      <InputTodo todoText={todoText} onChangeTodoText={onChangeTodoText} onClickAdd={onClickAdd}/>
+      <IncompleteTodos incompleteTodos={incompleteTodos} onClickComplete={onClickComplete} onClickDelete={onClickDelete}/>
+      <CompleteTodos completeTodos={completeTodos} onClickBack={onClickBack}/>
     </React.Fragment>
   );
 }
